8468df463580aff04b8f72da106a0faf389ce1ec
Jeffrey Macko <github.jm@macko.fr>
2020-04-14 12:18:50 +0200

    First commit

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "SimulatorControl",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SimulatorControl",
-            targets: ["SimulatorControl"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SimulatorControl",
-            dependencies: []),
-        .testTarget(
-            name: "SimulatorControlTests",
-            dependencies: ["SimulatorControl"]),
-    ]
+  name: "SimulatorControl",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "SimulatorControl",
+      targets: ["SimulatorControl"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/mrackwitz/Version.git", from: "0.7.2"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "SimulatorControl",
+      dependencies: ["Version"]),
+    .testTarget(
+      name: "SimulatorControlTests",
+      dependencies: ["SimulatorControl"]),
+  ]
 )
